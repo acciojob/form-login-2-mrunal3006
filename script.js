d4e066dfb4877bf089e8160bcdbe65c5aca70c45
mrunal3006 <128721188+mrunal3006@users.noreply.github.com>
2023-11-28 19:36:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function SubmitForm() {
+	var FirstName= document.getElementById('FirstName').value;
+	var LastName= document.getElementById('LastName').value;
+	var phoneNumber= document.getElementById('phoneNumber').value;
+	var email= document.getElementById('email').value;
+
+	var result= "First Name:" + FirstName + "/n" +
+		"Last Name:" + LastName + "/n" +
+		"Phone Number:" + phoneNumber + "/n" +
+		"Email ID:" + email;
+
+	alert(result);
+}
